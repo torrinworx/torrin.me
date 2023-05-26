c6b3e019cf7488859b4d62cbd87d934d484cfdb3
torrin1 <torrin@worx4you.com>
2023-05-26 18:59:54 -0400

    Various tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,10 @@ import React from "react";
 
 import Routes from "./Routes";
 import { ThemeWrapper } from "./Theme";
-import InteractiveSpheres from "./components/InteractiveSpheres";
 
 export const App = () => {
   return <ThemeWrapper>
-    <InteractiveSpheres>
       <Routes />
-    </InteractiveSpheres>
   </ThemeWrapper>
 };
 
---
+++ src/pages/Test1.jsx
@@ -0,0 +1,6 @@
+
+
+export const Test1 = () => {
+    return
+};
+
