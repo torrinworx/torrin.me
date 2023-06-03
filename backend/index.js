66c8b7d009fbe356c6a1852bb7efcbf1a121e2d3
torrin1 <torrin@worx4you.com>
2023-06-03 11:28:24 -0400

    Fixing port assignemtn issue

--- backend/index.js
+++ backend/index.js
@@ -25,5 +25,5 @@ app.get('*', (req, res) => {
     }
 });
 
-const port = process.env.BACKEND_PORT || 5000;
+const port = process.env.PORT || process.env.BACKEND_PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
