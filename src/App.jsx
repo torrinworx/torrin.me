8487c1f9dfc2a18a182f15920a48180b4ba4da6c
torrin1 <torrin@worx4you.com>
2023-06-03 11:53:33 -0400

    forgot

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import React from "react";
 import Routes from "./Routes";
 import { ThemeWrapper } from "./Theme";
 
-import api from "./Api";
-
 export const App = () => {
   return <ThemeWrapper>
       <Routes />
