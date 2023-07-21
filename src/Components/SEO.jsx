86940d88525a0b3eca402c9b7c4df78772a04b25
torrin1 <torrin@worx4you.com>
2023-07-21 13:07:15 -0400

    SEO test again

--- src/Components/SEO.jsx
+++ src/Components/SEO.jsx
@@ -62,6 +62,7 @@ const SEO = ({
             <meta name="keywords" content={keywords} />
             <link rel="canonical" href={url} />
             <meta name="robots" content="index, follow" />
+            <meta property="og:type" content="website" />
             <meta property="og:url" content={url} />
             <meta property="og:title" content={title} />
             <meta property="og:description" content={description} />
