d22d8484f93235ce86a0111c2baccdfd714e452b
torrin1 <torrin@worx4you.com>
2024-12-02 18:39:09 -0500

    implementing theme system for multi coloured dark/light modes and themes

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,7 +1,11 @@
-import { mount } from 'destam-dom';
-import { Button, Theme, Typography } from 'destamatic-ui';
+import { mount, Observer } from 'destam-dom';
+import { Button, Theme, Typography, Tabs, Radio } from 'destamatic-ui';
 
 import theme from './theme';
+import Personal from './components/Personal';
+import Portfolio from './components/Portfolio';
+import Collision from './components/Collision';
+import Wrap from './components/Wrap';
 
 const NotFound = () => <Theme value={theme}>
     <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
@@ -20,10 +24,36 @@ const NotFound = () => <Theme value={theme}>
     </div>
 </Theme>;
 
+
+
 const App = () => {
+    const currentTheme = Observer.mutable('light')
+    const themes = [
+        { label: 'Dark', value: 'dark' },
+        { label: 'Light', value: 'light' },
+    ];
+
+    currentTheme.watch(() => {
+        window.themeMode.set(currentTheme.get());
+        console.log(window.themeMode.get());
+    });
+
     return <Theme value={theme}>
-        Hello World!
-    </Theme>;
+        <Collision>
+            <Wrap>
+                <Radio items={themes} value={currentTheme} />
+
+                <Tabs style={{ width: '100%' }}>
+                    <mark:tab name='Portfolio'>
+                        <Portfolio />
+                    </mark:tab>
+                    <mark:tab name='Personal'>
+                        <Personal />
+                    </mark:tab>
+                </Tabs>
+            </Wrap>
+        </Collision>
+    </Theme >;
 };
 
 mount(document.body, window.location.pathname === '/' ? <App /> : <NotFound />);
