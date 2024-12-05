96ebdbcf1b3b7ebdeb5f5687aeafa3959315e646
torrin1 <torrin@worx4you.com>
2024-12-05 00:04:24 -0500

    Adding dynamic image loading

--- frontend/components/Personal.jsx
+++ frontend/components/Personal.jsx
@@ -1,35 +1,61 @@
-import { OArray } from "destam-dom";
+import { Observer, OObject, OArray } from "destam-dom";
+import { Shown } from "destamatic-ui";
 
 const ImageSection = ({ ws }) => {
 	const images = OArray([]);
 
 	const Image = ({ each: image }) => {
-		return <div>
-			<div style={{ height: '750px', margin: '10px', display: 'inline-block' }}>
-					<img src={`data:image/jpeg;base64,${image.data}`} style={{ width: '100%', height: '100%' }} />
-				</div>
+		const imageIndex = images.findIndex(img => img.fileName === image.fileName);
+		const showHighDef = Observer.mutable(false);
+		const imageObs = images[imageIndex];
+
+		const hideLowDef = Observer.mutable(false);
+
+		imageObs.observer.watch(() => {
+			console.log(imageObs.type);
+			if (imageObs.type === 'high') {
+				showHighDef.set(true);
+			}
+		});
+
+		return <div style={{ position: 'relative', height: '400px' }}>
+			<Shown value={imageObs} >
+				<img src={`data:image/jpeg;base64,${images[imageIndex].data}`} style={{ height: '400px', position: 'absolute', top: 0, left: 0 }} />
+			</Shown>
+			{showHighDef.map(show => show
+				? <img
+					onLoad={() => hideLowDef.set(true)}
+					src={`data:image/jpeg;base64,${images[imageIndex].data}`}
+					style={{ height: '400px', position: 'absolute', top: 0, left: 0 }}
+				/> : null
+			)}
 		</div>;
 	};
 
-    ws.send(JSON.stringify({ num: 10, index: 0 }));
+	// TODO: Find a dynamic way to load high def ones and image requests on scrolling down the page:
+	ws.send(JSON.stringify({ num: 10, index: 0 }));
 
-    ws.onmessage = (msg) => {
+	ws.onmessage = (msg) => {
 		try {
 			const message = JSON.parse(msg.data);
-			images.push(message);
+
+			if (message.type === 'low') {
+				if (!images.some(img => img.fileName === message.fileName && img.type === 'low')) {
+					images.push(OObject(message));
+				}
+			} else if (message.type === 'high') {
+				const imageIndex = images.findIndex(img => img.fileName === message.fileName);
+				images[imageIndex].data = message.data;
+				images[imageIndex].type = 'high'
+			}
 		} catch (e) {
 			console.error("Error parsing message:", e);
 		}
 	};
 
-	return <div>
-		<Image each={images} />
-	</div>;
+	return <Image each={images.observer.ignore('data').ignore('type')} />;
 };
 
 export default ({ ws }) => {
-	return <div>
-		Personal
-		<ImageSection ws={ws} />
-	</div>;
+	return <ImageSection ws={ws} />;
 };
