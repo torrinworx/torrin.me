ae2ce31c87d280d5f27d0ba24d1e2b63020f7dfa
torrin1 <torrin@worx4you.com>
2025-02-05 21:43:54 -0500

    Adding new backend images cache and streaming system

--- frontend/components/Gradient.jsx
+++ frontend/components/Gradient.jsx
@@ -14,12 +14,11 @@ export default Theme.use(theme => ({ children }) => {
 	const opacityTop = Observer.mutable(1);
 
 	gradient.watch(() => {
-		// If the transition is already running, immediately update for the new target
+		// If transition already running, immediately update for new target
 		let ongoingTransition = opacityTop.get() < 1;
-
 		nextGradient.set(gradient.get());
 
-		// Start transition to new gradient only if not ongoing
+		// Start transition only if not ongoing
 		if (!ongoingTransition) {
 			opacityTop.set(0);
 
@@ -30,27 +29,31 @@ export default Theme.use(theme => ({ children }) => {
 		}
 	});
 
-	return <>
-		<div style={{
-			position: 'absolute',
-			top: 0,
-			left: 0,
-			right: 0,
-			bottom: 0,
-			backgroundImage: currentGradient,
-			transition: 'opacity 250ms ease-in-out',
-			opacity: opacityTop,
-			zIndex: -1
-		}} />
-		<div style={{
-			position: 'absolute',
-			top: 0,
-			left: 0,
-			right: 0,
-			bottom: 0,
-			backgroundImage: nextGradient,
-			zIndex: -2
-		}} />
+	return <div style={{ position: "relative", zIndex: 0, minHeight: "100vh" }}>
+		<div
+			style={{
+				position: "fixed",
+				top: 0,
+				left: 0,
+				right: 0,
+				bottom: 0,
+				backgroundImage: currentGradient,
+				transition: "opacity 250ms ease-in-out",
+				opacity: opacityTop,
+				zIndex: -1,
+			}}
+		/>
+		<div
+			style={{
+				position: "fixed",
+				top: 0,
+				left: 0,
+				right: 0,
+				bottom: 0,
+				backgroundImage: nextGradient,
+				zIndex: -2,
+			}}
+		/>
 		{children}
-	</>
+	</div>;
 });
--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,5 +1,5 @@
 import { mount, Observer } from 'destam-dom';
-import { Button, Theme, Typography, Tabs, Radio, Switch, Paper } from 'destamatic-ui';
+import { Button, Theme, Typography, Tabs, Radio, Toggle, Paper } from 'destamatic-ui';
 
 import theme from './theme';
 import Personal from './components/Personal';
@@ -8,63 +8,64 @@ import Portfolio from './components/Portfolio';
 import Gradient from './components/Gradient';
 
 const NotFound = () => <Theme value={theme}>
-    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-        <Typography type='h4' style={{ marginBottom: '20px' }}>404 Page Not Found</Typography>
-        <Typography type='p1' style={{ marginBottom: '20px' }}>The page you are trying to access is either unavailable or restricted.</Typography>
-        <Button
-            type='contained'
-            label='Return to Site'
-            onMouseDown={() => {
-                state.client.observer.path('openPage').set({ page: "Landing" });
-                if (window.location.pathname !== '/') {
-                    window.location.href = '/';
-                }
-            }}
-        />
-    </div>
+	<div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+		<Typography type='h4' style={{ marginBottom: '20px' }}>404 Page Not Found</Typography>
+		<Typography type='p1' style={{ marginBottom: '20px' }}>The page you are trying to access is either unavailable or restricted.</Typography>
+		<Button
+			type='contained'
+			label='Return to Site'
+			onMouseDown={() => {
+				state.client.observer.path('openPage').set({ page: "Landing" });
+				if (window.location.pathname !== '/') {
+					window.location.href = '/';
+				}
+			}}
+		/>
+	</div>
 </Theme>;
 
+
 const App = ({ ws }) => {
-    return <Theme value={theme}>
-        <Gradient>
-            {/* <Collision> */}
-            <div style={{
-                padding: 40,
-                gap: 40,
-                display: 'flex',
-                flexDirection: 'column',
-            }}>
-                <Paper>
-                    <Switch
-                        value={Observer.mutable(true)}
-                        onChange={isChecked => {
-                            window.themeMode.set(isChecked ? 'dark' : 'light');
-                        }}
-                    />
-                    <Radio
-                        items={[
-                            { label: 'Red', value: 'red' },
-                            { label: 'Purple', value: 'purple' },
-                            { label: 'Cyan', value: 'cyan' },
-                            { label: 'Gold', value: 'gold' },
-                        ]}
-                        value={window.colorMode}
-                    />
-                </Paper>
-                <Paper>
-                    <Tabs style={{ width: '100%' }}>
-                        <mark:tab name='Personal'>
-                            <Personal ws={ws} />
-                        </mark:tab>
-                        <mark:tab name='Portfolio'>
-                            <Portfolio />
-                        </mark:tab>
-                    </Tabs>
-                </Paper>
-            </div>
-            {/* </Collision> */}
-        </Gradient>
-    </Theme>;
+	const SelectRadio = Radio(window.colorMode);
+
+	return <Theme value={theme}>
+		<Gradient>
+			{/* <Collision> */}
+			<div style={{
+				padding: 40,
+				gap: 40,
+				display: 'flex',
+				flexDirection: 'column',
+			}}>
+				<Paper>
+					<div theme='center_row' style={{ gap: 8 }}>
+
+						<SelectRadio style={{ background: '$color_red' }} value={'red'} />
+						<SelectRadio style={{ background: '$color_purple' }} value={'purple'} />
+						<SelectRadio style={{ background: '$color_cyan' }} value={'cyan'} />
+						<SelectRadio style={{ background: '$color_gold' }} value={'gold'} />
+						<Toggle
+							value={Observer.mutable(true)}
+							onChange={isChecked => {
+								window.themeMode.set(isChecked ? 'dark' : 'light');
+							}}
+						/>
+					</div>
+				</Paper>
+				<Paper>
+					<Tabs style={{ width: '100%' }}>
+						<mark:tab name='Personal'>
+							<Personal ws={ws} />
+						</mark:tab>
+						<mark:tab name='Portfolio'>
+							<Portfolio />
+						</mark:tab>
+					</Tabs>
+				</Paper>
+			</div>
+			{/* </Collision> */}
+		</Gradient>
+	</Theme>;
 };
 
 window.addEventListener("load", () => {
