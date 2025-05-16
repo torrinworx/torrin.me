3c81e2f818ea7f9eb08fdacd11c1d9e4678c737a
torrin1 <torrin@worx4you.com>
2025-05-16 18:43:34 -0400

    Fixing some theme stuff

--- frontend/theme.js
+++ frontend/theme.js
@@ -1,85 +1,118 @@
 import { atomic } from 'destam/Network';
 import { OObject, Observer } from 'destam-dom';
 
+const mainColors = {
+	$color_white: '#D6D6D6',
+	$color_black: '#141414',
+	$color_red: '#A30029',
+	$color_purple: '#6021c0',
+	$color_cyan: '#368F8B',
+	$color_gold: '#FABC2A',
+};
+
 const colorModes = {
 	red: {
-		$color: '$color_red',
+		$color: mainColors.$color_red,
 	},
 	purple: {
-		$color: '$color_purple',
+		$color: mainColors.$color_purple,
 	},
 	cyan: {
-		$color: '$color_cyan',
+		$color: mainColors.$color_cyan,
 	},
 	gold: {
-		$color: '$color_gold',
+		$color: mainColors.$color_gold,
 	},
 };
 
 const themeModes = {
 	light: {
-		$color_main: '$color_white',
-		$color_text: '$color',
-		$color_top: '$color',
-		$color_hover: '$shiftBrightness($color_white, 0.1)',
+		$color_main: '$color',
+		$color_top: '$color_white',
 		$color_hover_top: '$color_white',
+		$color_grad_tr: '$color_white',
+		$color_grad_bl: '$color',
 	},
 
 	dark: {
 		$color_main: '$color',
-		$color_text: '$color_white',
 		$color_top: '$color_white',
-		$color_hover: '$shiftBrightness($color_white, 0.1)',
 		$color_hover_top: '$color_white',
-	},
+		$color_grad_tr: '$color_black',
+		$color_grad_bl: '$color',
+	}
 };
 
 const transition = '250ms ease-in-out';
 
 const theme = OObject({
-	'*': OObject({
+	// destamatic-ui
+	'*': OObject({ // Doesn't have to be an oobject I don't think
 		fontFamily: 'IBM Plex Sans',
 		fontWeight: 600,
 		boxSizing: 'border-box',
 		transition: `opacity ${transition}, box-shadow ${transition}, background-color ${transition}, color ${transition}, border-color ${transition}`,
-		$color_white: '#D6D6D6',
-		$color_black: '#141414',
-		$color_red: '#A30029',
-		$color_purple: '#6021c0',
-		$color_cyan: '#368F8B',
-		$color_gold: '#FABC2A',
+		...mainColors,
 	}),
 
 	// Override destamatic-ui primary theme
 	primary: {
-		extends: ['*'],
-		$color: '$color',
-		$color_hover: '$color_hover',
+		$color: '$color_main',
+		$color_text: '$contrast_text($color_main)',
+		$color_hover: '$saturate($shiftBrightness($color_main, -.3), -.3)',
 		$color_top: '$color_top',
 	},
 
-	radius: {
-		$radius: '20px',
-		borderRadius: '$radius',
+	// Need to handle the $color_top slot here specially when switching between light/dark mode?
+	gradient: OObject({
+		extends: ['*'],
+		$gradientCSS: 'linear-gradient(to top right, $color_grad_bl, $color_grad_tr)',
+	}),
+
+	shadow: {
+		boxShadow: '4px 4px 10px $alpha($color_top, 0.2)',
 	},
 
 	paper: {
-		extends: ['*', 'radius', 'shadow'],
-		padding: 10,
-		background: '$alpha($color, 0.4)',
+		extends: ['*', 'radius'],
+		color: '$color_main',
 		maxWidth: 'inherit',
 		maxHeight: 'inherit',
+		padding: 10,
+		background: '$alpha($color_main, 0.4)',
 		blur: '25px',
 	},
 
-	// The issue with the gradient not updating to the right color isn't an issue with the theme,
-	// because other components like typography work just fine with updating $color_text.
-	// it's specifically an issue with how gradient is setup to react to variable changes
-	// in the theme.
-	gradient: OObject({
-		extends: ['*'],
-		$gradientCSS: 'linear-gradient(to top right, $color_main, $color_top)',
-	}),
+	radius: {
+		borderRadius: 16
+	},
+
+	toggle: {
+		display: 'flex',
+		alignItems: 'center',
+		cursor: 'pointer',
+		overflow: 'clip',
+		position: 'relative',
+		width: '60px',
+		height: '30px',
+		background: '$color_top',
+		borderRadius: '37.5px',
+	},
+
+	toggle_hovered: {
+		background: '$color_hover'
+	},
+
+	toggleknob: {
+		position: 'absolute',
+		top: '50%',
+		transform: 'translateX(4px) translateY(-50%) scale(1)',
+		width: '23px',
+		height: '23px',
+		background: '$color_main',
+		borderRadius: '50%',
+		transition: 'transform 150ms cubic-bezier(0.4, 0.0, 0.2, 1), background-color 150ms ease-in-out',
+	},
 
 	typography: {
 		color: '$color_text',
@@ -117,6 +150,132 @@ const theme = OObject({
 		color: '$color_text',
 		fontSize: 14
 	},
+	typography_regular: { fontStyle: 'normal' },
+	typography_bold: { fontWeight: 'bold' },
+	typography_italic: { fontStyle: 'italic' },
+	typography_center: { textAlign: 'center' },
+	typography_inline: { display: 'inline' },
+
+	button: {
+		extends: 'center_radius',
+		padding: '10px 15px',
+		userSelect: 'none',
+		border: 'none',
+		cursor: 'pointer',
+		textDecoration: 'none',
+		position: 'relative',
+		overflow: 'clip',
+		color: '$color_top',
+		boxShadow: 'none',
+		background: 'none',
+		_cssProp_focus: { outline: 'none' },
+	},
+
+	button_text_hovered: {
+		background: 'none',
+		color: '$color_hover'
+	},
+
+	button_contained: {
+		extends: 'typography_p1_bold',
+		background: '$color_top',
+		color: '$color_main',
+	},
+
+	button_contained_hovered: {
+		background: '$color_hover',
+		color: '$color_hover_top'
+	},
+
+	button_outlined: {
+		extends: 'typography_p1_bold',
+		borderWidth: 2,
+		borderStyle: 'solid',
+		borderColor: '$color_top',
+		color: '$color_top',
+	},
+
+	button_outlined_hovered: {
+		color: '$color_hover',
+		borderColor: '$color_hover',
+	},
+
+	button_outlined_disabled: {
+		borderColor: '$saturate($color, -1)',
+		color: '$saturate($color, -1)',
+	},
+
+	button_contained_disabled: {
+		$bg: '$saturate($color, -1)',
+		background: '$bg',
+		color: '$contrast_text($bg)',
+	},
+
+	button_icon: {
+		color: '$color_top',
+	},
+
+	button_icon_hovered: {
+		color: '$color_hover',
+	},
+
+	text: {
+		extends: 'typography_p1_regular',
+		color: '$color_top'
+	},
+
+	text_hovered: {
+		color: '$color_hover'
+	},
+
+	focusable: {
+		borderStyle: 'solid',
+		borderWidth: 2,
+		borderColor: '$color_top',
+		transitionDuration: '0.3s',
+		transitionProperty: 'border-color, background-color, box-shadow',
+	},
+
+	focused: {
+		boxShadow: '$color_hover 0 0 0 0.2rem',
+	},
+
+	field: {
+		extends: 'radius_typography_p1_regular_focusable',
+		outline: 0,
+		padding: 10,
+		background: '$color_main',
+		color: '$color_top',
+	},
+
+	loadingDots_dot: {
+		_keyframes_animationName: `
+			0%, 100% { opacity: 0; }
+			50% { opacity: 1; }
+		`,
+
+		background: '$color_top',
+		display: 'inline-block',
+		width: '8px',
+		height: '8px',
+		borderRadius: '50%',
+		animationName: '$animationName',
+		animationDuration: '1s',
+		animationIterationCount: 'infinite',
+		animationTimingFunction: 'ease-in-out',
+		margin: '20px 4px',
+	},
+
+	// custom
+	page: {
+		background: '$color_main',
+		padding: '40px',
+		gap: '40px',
+		display: 'flex',
+		flexDirection: 'column',
+		height: '100%',
+		minHeight: '100vh'
+	},
 });
 
 window.colorMode = Observer.mutable('red');
@@ -131,6 +290,7 @@ window.colorMode.effect(color => atomic(() => {
 
 window.themeMode.effect(mode => atomic(() => {
 	mode = mode ? 'dark' : 'light'; // if mode is true => dark mode, else false => light mode
+	console.log(mode);
 	for (const [key, val] of Object.entries(themeModes[mode])) {
 		theme['*'][key] = val;
 	}
