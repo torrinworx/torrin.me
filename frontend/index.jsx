7653120d2fe486518fde530fe22fb044a2776cbf
torrin1 <torrin@worx4you.com>
2025-05-16 18:43:34 -0400

    Fixing some theme stuff

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,5 +1,5 @@
-import { mount } from 'destam-dom';
-import { Button, Theme, Typography, Radio, Toggle, Paper, Gradient } from 'destamatic-ui';
+import { mount, Observer } from 'destam-dom';
+import { Button, Theme, Typography, Radio, Toggle, Paper, Gradient, TextField } from 'destamatic-ui';
 
 import theme from './theme';
 import Portfolio from './components/Portfolio';
@@ -28,7 +28,7 @@ const App = () => {
 	return <Theme value={theme.theme}>
 		<Gradient>
 			{/* <Collision> */}
-			<div theme='center' style={{ paddingTop: 20 }}>
+			<div theme='center' style={{ paddingTop: 20, userSelect: 'none' }}>
 				<Paper style={{ width: 250 }}>
 					<div theme='center_row' style={{ gap: 8 }}>
 						<SelectRadio style={{ color: '$color_red' }} value={'red'} />
@@ -50,6 +50,33 @@ const App = () => {
 				<Paper>
 
 					<Portfolio />
+				</Paper>
+
+				<Paper>
+					<Typography type='h5' label='UI Component Test:' />
+
+					<Typography type='h1' label='Header 1' />
+					<Typography type='h2' label='Header 2' />
+					<Typography type='h3' label='Header 3' />
+					<Typography type='h4' label='Header 4' />
+					<Typography type='h5' label='Header 5' />
+					<Typography type='h6' label='Header 6' />
+					<Typography type='p1' label='Paragraph 1' />
+					<Typography type='p2' label='Paragraph 2' />
+					<Typography type='p1_regular' label='Paragraph 1 Regular' />
+					<Typography type='p1_bold' label='Paragraph 1 Bold' />
+					<Typography type='p1_italic' label='Paragraph 1 Italic' />
+					<div theme='row' style={{ gap: 10 }}>
+						<Button type='contained' label='Button' onClick={() => { }} />
+						<Button type='outlined' label='Button' onClick={() => { }} />
+						<Button type='text' label='Button' onClick={() => { }} />
+					</div>
+					<div theme='column' style={{ gap: 10 }} >
+						<TextField placeholder='Email' value={Observer.mutable('')} />
+						<TextField />
+						<TextField />
+					</div>
+					<Toggle value={Observer.mutable(false)} />
 				</Paper>
 			</div>
 			{/* </Collision> */}
