8d99026cc79327bd913022766cf441cdadb31a1c
torrin1 <torrin@worx4you.com>
2025-05-19 23:19:35 -0400

    updating build file

--- build.sh
+++ build.sh
@@ -9,23 +9,58 @@ command_exists() {
 	command -v "$1" >/dev/null 2>&1
 }
 
+# Install git if not present
 if ! command_exists git; then
 	sudo apt-get update
 	sudo apt-get install -y git
 fi
 
 git submodule init
-git submodule update
+git submodule update --init --recursive
 
-if ! command_exists nvm; then
+# Install nvm and Node.js
+install_nvm_and_node() {
+	echo "Installing NVM..."
 	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
-	export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+	export NVM_DIR="$HOME/.nvm"
+	if [ -s "$NVM_DIR/nvm.sh" ]; then
+		source "$NVM_DIR/nvm.sh"
+	else
+		echo "nvm.sh not found. Exiting."
+		exit 1
+	fi
+
+	if [ -s "$NVM_DIR/bash_completion" ]; then
+		source "$NVM_DIR/bash_completion"
+	fi
+
+	echo "Installing Node.js version 23..."
+	nvm install 23
+	nvm use 23
+
+	# Verify installations
+	echo "NVM version:"
+	nvm --version
+
+	echo "Node version:"
+	node --version
+
+	echo "NPM version:"
+	npm --version
+}
+
+# Check if Node.js and NPM are installed
+if command_exists node && command_exists npm; then
+	echo "Node and NPM are already installed."
+	node --version
+	npm --version
+else
+	echo "Node or NPM not found. Attempting to install via NVM."
+	install_nvm_and_node
 fi
 
-nvm install 23
-nvm use 23
-
+# Clean build directory and zip file if they exist from previous run
 if [ -d "$BUILD_DIR" ]; then
 	rm -rf "$BUILD_DIR"
 fi
@@ -36,9 +71,8 @@ fi
 
 # Install npm dependencies and build the project
 npm install
+# npm run test # TODO: Enable when destamatic tests are working
 npx vite build
-
-rm -rf ./destamatic-ui/.git
 
 # Prepare build directory
 mkdir -p "$BUILD_DIR"
@@ -50,10 +84,14 @@ cp -r ./node_modules "$BUILD_DIR"
 cp ./package.json "$BUILD_DIR"
 cp ./package-lock.json "$BUILD_DIR"
 
-# Create the run script in the build directory
+# Create the run script the service will use
 cat << 'EOF' > "$BUILD_DIR/run.sh"
 #!/bin/bash
-node ./backend/index.js
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+. ~/.nvm/nvm.sh use 23
+node "$SCRIPT_DIR/backend/index.js"
 EOF
 
 chmod +x "$BUILD_DIR/run.sh"
+
+zip -r "$ZIP_FILE" "$BUILD_DIR"
