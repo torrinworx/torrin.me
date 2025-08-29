0ea0ef4dff77d345b8863af94f1cd16e3782bd90
torrin1 <torrin@worx4you.com>
2025-08-29 17:47:31 -0400

    Seo fixes, button fixes, adding button href,  consolodating sections into Card,  updating section text

--- frontend/Blog.jsx
+++ frontend/Blog.jsx
@@ -4,19 +4,18 @@ const Blog = ({ stage }) => {
     const blogs = Object.values(stage.props.blogs); // somehow sort by date, most recent at top?
 
     const Card = ({ each }) => {
+        const nameWithoutExtension = each.name.replace(/\.[^/.]+$/, '');
+
         return <Paper theme='column_fill'>
             <Typography type='h4' label={each.header} />
             <Typography type='p1' label={each.description} />
-            <Button type='outlined' label='View' onClick={() => {
-                const nameWithoutExtension = each.name.replace(/\.[^/.]+$/, "");
-                stage.open({ name: "blog/" + nameWithoutExtension });
-            }} />
+            <Button type='outlined' label='View' onClick={() => stage.open({ name: 'blog/' + nameWithoutExtension })} href={`/blog/${nameWithoutExtension}`}/>
         </Paper>;
     };
 
     return <>
         <div theme='row'>
-            <Button type='outlined' label='Back' onClick={() => stage.open({ name: 'landing' })} />
+            <Button type='outlined' label='Back' onClick={() => stage.open({ name: 'landing' })} href='/' />
         </div>
         <Card each={blogs} />
     </>;
