0d2b6396c6ad2070bba7d377be265b01fe268ea0
Torrin Leonard <torrin@torrin.me>
2025-12-06 11:00:00 -0500

    Modifying Blog to use new Stage.jsx system

--- frontend/pages/Blog.jsx
+++ frontend/pages/Blog.jsx
@@ -1,10 +1,8 @@
 import { Typography, Button, Paper, StageContext, LoadingDots, suspend, Default, Stage } from 'destamatic-ui';
 import Markdown from '../utils/Markdown';
 
-/*
-// This can be it's own file:
-const Something = StageContext.use(s => suspend(LoadingDots, async ({ key, value }) => {
-    s.props.enabled.set(false);
+const BlogPage = StageContext.use(s => suspend(LoadingDots, async ({ key, value }) => {
+    s.parent.props.enabled.set(false);
     let content = await fetch(`/blog/${key}`).then(r => r.text());
 
     const cleanupMd = (md) => {
@@ -39,40 +37,6 @@ const Something = StageContext.use(s => suspend(LoadingDots, async ({ key, value
     </div>;
 }));
 
-const blogPages = Object.entries(blogs).reduce((acc, [key, value]) => {
-    const baseName = key.replace(/\.[^/.]+$/, '');
-    const routeKey = `blog/${baseName}`;
-    acc[baseName] = () => <Something key={key} value={value} />;
-    return acc;
-}, {});
-
-
-
-const Blog = StageContext.use(stage => () => {
-    const blogs = Object.values(stage.props.blogs); // somehow sort by date, most recent at top?
-
-    const Card = ({ each }) => {
-        const nameWithoutExtension = each.name.replace(/\.[^/.]+$/, '');
-
-        return <Paper theme='column_fill'>
-            <Typography type='h4' label={each.header} />
-            <Typography type='p1' label={each.description} />
-            <Button type='outlined' label='View' onClick={() => stage.open({ name: 'blog/' + nameWithoutExtension })} href={`/blog/${nameWithoutExtension}`}/>
-        </Paper>;
-    };
-
-    return <>
-        <div theme='row'>
-            <Button type='outlined' label='Back' onClick={() => stage.open({ name: 'landing' })} href='/' />
-        </div>
-        <Card each={blogs} />
-    </>;
-});
-
-export default Blog;
-*/
-
-
 const BlogLanding = StageContext.use(stage => () => {
     const blogs = Object.values(stage.globalProps.blogs); // TODO: sort by date, most recent at top?
 
@@ -97,17 +61,23 @@ const BlogLanding = StageContext.use(stage => () => {
 });
 
 const Blog = suspend(LoadingDots, async () => {
-
     const response = await fetch('/blog/index.json');
     const blogs = await response.json();
+
     const BlogsConfig = {
         acts: {
-            Blog: BlogLanding,
+            blog: BlogLanding,
+            ...Object.entries(blogs).reduce((acc, [key, value]) => {
+                const baseName = key.replace(/\.[^/.]+$/, '');
+                acc[baseName] = () => <BlogPage key={key} value={value} />;
+                return acc;
+            }, {})
         },
-        initial: 'Blog',
+        initial: 'blog',
         template: Default,
         blogs,
     }
+    console.log(BlogsConfig.acts);
 
     return <StageContext value={BlogsConfig}>
         <Stage />
