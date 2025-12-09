3f29c8d01e2976945a4d2ad498ee0b97a285bb7b
Torrin Leonard <torrin@torrin.me>
2025-12-09 15:53:53 -0500

    Fixing blog page routing issue

--- frontend/pages/Blog.jsx
+++ frontend/pages/Blog.jsx
@@ -2,10 +2,12 @@ import { Typography, Button, Paper, StageContext, LoadingDots, suspend, Default,
 import Markdown from '../utils/Markdown';
 import NotFound from './NotFound';
 
-const BlogPage = StageContext.use(s => suspend(LoadingDots, async ({ key, value }, cleanup) => {
+const BlogPage = StageContext.use(s => suspend(LoadingDots, async (_, cleanup) => {
+    const slug = s.current;
+    const meta = s.blogs?.[`${slug}.md`] || s.blogs?.[slug];
     s.parent.props.enabled.set(false);
-    console.log(s.parent.props.enabled.get());
-    let content = await fetch(`/blog/${key}`).then(r => r.text());
+    const response = await fetch(`/blog/${meta.name}`);
+    let content = await response.text();
 
     const cleanupMd = (md) => {
         md = md.replace(/#\s*header\s*\n([^#]*)\n+/i, '');
@@ -27,24 +29,34 @@ const BlogPage = StageContext.use(s => suspend(LoadingDots, async ({ key, value 
         }).format(date);
     };
 
-    cleanup(() => s.parent.props.enabled.set(true))
+    cleanup(() => s.parent.props.enabled.set(true));
 
-    return <div theme='column' style={{ gap: 40 }}>
-        <div theme='row_spread'>
-            <Button type='outlined' label='Back' onClick={() => s.open({ name: 'blog' })} />
+    return (
+        <div theme='column' style={{ gap: 40 }}>
+            <div theme='row_spread'>
+                <Button
+                    type='outlined'
+                    label='Back'
+                    onClick={() => s.open({ name: 'blog' })}
+                />
+            </div>
+            <Paper>
+                <Typography
+                    type='p1'
+                    label={`Created on: ${formatDate(meta.created)}`}
+                />
+                <Typography
+                    type='p1'
+                    label={`Modified on: ${formatDate(meta.modified)}`}
+                />
+                <Markdown value={content} />
+            </Paper>
         </div>
-        <Paper>
-            <Typography type='p1' label={`Created on: ${formatDate(value.created)}`} />
-            <Typography type='p1' label={`Modified on: ${formatDate(value.modified)}`} />
-            <Markdown value={content} />
-        </Paper>
-    </div>;
+    );
 }));
 
 const BlogLanding = StageContext.use(stage => () => {
-    console.log(stage)
-    const blogs = Object.values(stage.props.blogs); // TODO: sort by date, most recent at top?
-
+    const blogs = Object.values(stage.blogs); // TODO: sort by date, most recent at top?
     const Card = ({ each }) => {
         const name = each.name.replace(/\.[^/.]+$/, '');
 
@@ -65,31 +77,36 @@ const BlogLanding = StageContext.use(stage => () => {
     </>;
 });
 
-const Blog = suspend(LoadingDots, async () => {
-    const response = await fetch('/blog/index.json');
-    const blogs = await response.json();
-    const blogPages = Object.entries(blogs).reduce((acc, [key, value]) => {
-        const baseName = key.replace(/\.[^/.]+$/, '');
-        acc[baseName] = () => <BlogPage key={key} value={value} />;
-        return acc;
-    }, {});
-
+// Suspending the loading of the stage context overrides the .open that the parent stage context sends through micro task queue.
+const Blog = () => {
     const BlogsConfig = {
         acts: {
             blog: BlogLanding,
-            fallback: NotFound,
-            ...blogPages,
+            // fallback: NotFound,
         },
         initial: 'blog',
         template: Default,
-        blogs,
-        ssg: true,
-        urlRouting: true,
     }
 
+    const BlogLoader = StageContext.use(s => suspend(LoadingDots, async () => {
+        const response = await fetch('/blog/index.json');
+        const blogs = await response.json();
+
+        const blogPages = Object.entries(blogs).reduce((acc, [key, value]) => {
+            const baseName = key.replace(/\.[^/.]+$/, '');
+            acc[baseName] = BlogPage;
+            return acc;
+        }, {});
+
+        Object.assign(s.acts, blogPages);
+        s.blogs = blogs;
+
+        return <Stage />
+    }));
+
     return <StageContext value={BlogsConfig}>
-        <Stage />
+        <BlogLoader />
     </StageContext>
-});
+};
 
 export default Blog;
