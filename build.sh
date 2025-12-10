99c53206323c3ac581c8f1a89966853a7a7beb72
Torrin Leonard <torrin@torrin.me>
2025-12-09 22:48:15 -0500

    Updating build and server

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ rm -R ./build
 vite build
 vite build --config vite.config.ssg.js 
 node ./destamatic-ui/ssg/build.js ./build ./frontend
-# npx esbuild ./server.js --bundle --platform=node --target=node23 --outfile=./build/index.js --format=esm --external:fs --external:path --external:os --external:crypto --external:vite --external:lightningcss
+npx esbuild ./server.js --bundle --platform=node --target=node25 --outfile=./build/server.js --format=esm --external:fs --external:path --external:os --external:crypto --external:vite --external:lightningcss
