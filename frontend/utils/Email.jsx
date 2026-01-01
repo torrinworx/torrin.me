8152cf1a3c314804381e9830fc4ce6454484ce7f
Torrin Leonard <torrin@torrin.me>
2026-01-01 10:55:22 -0500

    Moving email to component

---
+++ frontend/utils/Email.jsx
@@ -0,0 +1,29 @@
+import { Button, Icon, Observer } from 'destamatic-ui'
+
+const Email = () => {
+    const emailCheck = Observer.mutable(false);
+    emailCheck.watch(() => {
+        if (emailCheck.get()) {
+            setTimeout(() => {
+                emailCheck.set(false);
+            }, 5000);
+        }
+    });
+
+    return <Button
+        title='Copy email to clipboard.'
+        type='outlined'
+        iconPosition='right'
+        label={emailCheck.map(c => c ? 'Copied!' : 'Email')}
+        icon={emailCheck.map(c => c
+            ? <Icon name='feather:check' />
+            : <Icon name='feather:copy' />)}
+        onClick={async () => {
+            emailCheck.set(true);
+            await navigator.clipboard.writeText('torrin@torrin.me')
+        }}
+        loading={false}
+    />;
+};
+
+export default Email;
