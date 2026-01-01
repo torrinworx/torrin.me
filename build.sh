54a196feebadb31c279df2212ead32337138d033
Torrin Leonard <torrin@torrin.me>
2026-01-01 11:37:37 -0500

    More fixes

--- build.sh
+++ build.sh
@@ -15,32 +15,24 @@ vite build --config vite.config.ssg.js
 # SSG step
 node ./destamatic-ui/ssg/build.js "$BUILD_DIR" ./frontend https://torrin.me
 
-# Bundle server
-npx esbuild ./server.js \
-  --bundle \
-  --platform=node \
-  --target=node25 \
-  --outfile="$BUILD_DIR/server.js" \
-  --format=esm \
-  --external:node:* \
-  --external:dotenv \
-  --external:nodemailer \
-  --external:vite \
-  --external:lightningcss
+# Server
+cp ./server.js "$BUILD_DIR/server.js"
+cp ./package.json "$BUILD_DIR/package.json"
+cp ./package-lock.json "$BUILD_DIR/package-lock.json"
 
 # Create run.sh inside build
 cat << 'EOF' > "$BUILD_DIR/run.sh"
 #!/bin/bash
+npm i --production
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-. ~/.nvm/nvm.sh use 23
+. ~/.nvm/nvm.sh use 25
 node "$SCRIPT_DIR/server.js"
 EOF
 
 chmod +x "$BUILD_DIR/run.sh"
 
 # Create zip with everything inside build/
-
-  zip -r "$ZIP_FILE" "$BUILD_DIR"
+zip -r "$ZIP_FILE" "$BUILD_DIR"
 
 # Show sizes
 du -sh "$BUILD_DIR"
